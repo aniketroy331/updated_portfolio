17c1c6f55e3c6b67ba08461dcf7f2a55068477b0
Aniket Roy <aniketroy10100@gmail.com>
2025-03-31 00:49:24 +0530

    Delete public directory

--- public/js/header.js
+++
@@ -1,7 +0,0 @@
-// Toggle Mobile Menu
-const mobileMenu = document.getElementById('mobile-menu');
-const navbarMenu = document.querySelector('.navbar-menu');
-
-mobileMenu.addEventListener('click', () => {
-  navbarMenu.classList.toggle('active');
-});
